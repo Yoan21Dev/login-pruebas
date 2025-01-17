9006ae03ff7f814abf26a29b75eb9b887c879fc1
Yarm <yoander@gmail.com>
2025-01-17 16:27:44 -0400

    redis eliminado todo en cache de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import type { RedisClientOptions } from 'redis';
 import { CacheModule } from '@nestjs/cache-manager';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { redisStore } from 'cache-manager-redis-store';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { EmailModule } from '@core/email/email.module';
@@ -17,16 +15,10 @@ import { UserAdminModule } from '@user_admin/user_admin.module';
 
 @Module({
   imports: [
-    CacheModule.registerAsync<RedisClientOptions>({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        store: redisStore,
-        url: configService.get('REDIS_URL'),
-        username: configService.get('REDIS_USERNAME'),
-        password: configService.get('REDIS_PASSWORD'),
-      }),
+    CacheModule.register({
       isGlobal: true,
-      inject: [ConfigService],
+      // Aquí se puedes agregar opciones de caché si lo necesitas redis etc 
+
     }),
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     ScheduleModule.forRoot(),
@@ -45,4 +37,4 @@ import { UserAdminModule } from '@user_admin/user_admin.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule { }
